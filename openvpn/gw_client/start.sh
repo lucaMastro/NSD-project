f18821e994e09c298be8c2d76bf7da615ab284db
lucaMastro <luca.mastrobattista@alumni.uniroma2.eu>
2024-02-20 15:41:19 +0100

    pushed gw_client's files

---
+++ openvpn/gw_client/start.sh
@@ -0,0 +1,14 @@
+#IP Configuration:
+sysctl -w net.ipv4.ip_forward=1
+
+ip addr add 192.168.0.2/24 dev eth0
+ip addr add 4.1.0.2/30 dev eth1
+ip route add default via 4.1.0.1
+
+iptables -A POSTROUTING -t nat -o eth1 -j MASQUERADE
+
+# start ovpn
+openvpn /root/ovpn/gw_client.ovpn &
+
+cd
+bash
